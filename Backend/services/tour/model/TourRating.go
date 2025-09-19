fd4cd2afe2f4912d6a9ad5c90035f0226c17acd6
Mihailo84 <mihailo.vukovic2002@gmail.com>
2025-09-19 18:54:21 +0200

    Added images with their preview and also added the view for all tour ratings

--- Backend/services/tour/model/TourRating.go
+++ Backend/services/tour/model/TourRating.go
@@ -7,13 +7,16 @@ import (
 )
 
 type TourRating struct {
-	Id           uuid.UUID `json:"id" bson:"_id,omitempty"`
-	TourId       uuid.UUID `json:"tourId" bson:"tourId"`
-	Rating       int       `json:"rating" bson:"rating"`
-	Comment      string    `json:"comment,omitempty" bson:"comment,omitempty"`
-	TouristName  string    `json:"touristName,omitempty" bson:"touristName,omitempty"`
-	TouristEmail string    `json:"touristEmail,omitempty" bson:"touristEmail,omitempty"`
-	VisitedAt    time.Time `json:"visitedAt,omitempty" bson:"visitedAt,omitempty"`
-	CommentedAt  time.Time `json:"commentedAt,omitempty" bson:"commentedAt,omitempty"`
-	CreatedAt    time.Time `json:"createdAt" bson:"createdAt"`
+  Id           uuid.UUID `json:"id" bson:"_id,omitempty"`
+  TourId       uuid.UUID `json:"tourId" bson:"tourId"`
+  Rating       int       `json:"rating" bson:"rating"`
+  Comment      string    `json:"comment,omitempty" bson:"comment,omitempty"`
+  TouristName  string    `json:"touristName,omitempty" bson:"touristName,omitempty"`
+  TouristEmail string    `json:"touristEmail,omitempty" bson:"touristEmail,omitempty"`
+  VisitedAt    time.Time `json:"visitedAt,omitempty" bson:"visitedAt,omitempty"`
+  CommentedAt  time.Time `json:"commentedAt,omitempty" bson:"commentedAt,omitempty"`
+  CreatedAt    time.Time `json:"createdAt" bson:"createdAt"`
+
+  Images       []string  `json:"images,omitempty" bson:"images,omitempty"`
 }
+
